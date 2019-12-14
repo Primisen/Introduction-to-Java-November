719ce5968484a749c3efa5fab564c3b9c6543d79
Nadia Shokel <primisen17@gmail.com>
2019-12-14 04:56:25 +0300

    Delete RootVegetableType.java

--- part4/ingredient/vegetable/category/root/type/RootVegetableType.java
+++
@@ -1,9 +0,0 @@
-package com.primisen.java_november.part4.ingredient.vegetable.category.root.type;
-
-public enum RootVegetableType {
-    CARROT, //морковь
-    POTATOES,
-    BEET, //свекла
-    BOW, //лук
-    GARLIC //чеснок
-}
