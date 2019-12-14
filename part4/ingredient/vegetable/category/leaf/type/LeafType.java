78a0d1a748d5d827f443a13817fa5270ea04d096
Nadia Shokel <primisen17@gmail.com>
2019-12-14 04:57:22 +0300

    Delete LeafType.java

--- part4/ingredient/vegetable/category/leaf/type/LeafType.java
+++
@@ -1,8 +0,0 @@
-package com.primisen.java_november.part4.ingredient.vegetable.category.leaf.type;
-
-public enum LeafType {
-    PARSLEY, //петрушка
-    SPINACH, //шпинат
-    SORREL, //щавель
-    NETTLE //крапива
-}
