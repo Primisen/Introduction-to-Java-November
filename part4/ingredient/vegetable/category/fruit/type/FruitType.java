83c64af07c96bafbf4e6bb0d3e00a363b7a3b3f5
Nadia Shokel <primisen17@gmail.com>
2019-12-14 04:56:48 +0300

    Delete FruitType.java

--- part4/ingredient/vegetable/category/fruit/type/FruitType.java
+++
@@ -1,9 +0,0 @@
-package com.primisen.java_november.part4.ingredient.vegetable.category.fruit.type;
-
-public enum FruitType {
-    CUCUMBER, //огурец
-    EGGPLANT, //баклажан
-    CORN, //кукуруза
-    BEANS, //фасоль
-    BELL_PEPPER //болгарский перец
-}
